87a47e70eda6eea0a6d55fd29e7d580da4c9b20a
Galitan-dev <galitan.dev@gmail.com>
2021-07-19 21:09:00 +0200

    debug

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Mongoose error:"));
-db.once('open', () => {
+db.once('open', async () => {
     console.log("Connected!");
     console.log(await db.collection("users").find().exec());
 });
