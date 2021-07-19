f48fb0ef18c25fe0c975c5deb8a101a119653964
Galitan-dev <galitan.dev@gmail.com>
2021-07-19 16:02:11 +0200

    req logs

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ db.once('open', () => {
     console.log("Connected!");
 });
 
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+});
 app.use(express.static(path.join(__dirname, 'public'), { extensions: [ "html", "js", "css" ] }));
 
 app.get("/api/:category/:endpoint", async (req, res) => {
