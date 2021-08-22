4d0cb9c696962ce1cc48f950b282627b051d7de6
Galitan-dev <galitan.dev@gmail.com>
2021-08-22 19:18:17 +0200

    scripts

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 NAME=$1
+ID=$(mongo heberg --eval "db.hebergs.findOne({name:'$NAME'}).containerId" --quiet)
 
-tail -f $HOME/hosts/$NAME/out.log
+tail -f "/var/lib/docker/containers/$ID/$ID-json.log"
