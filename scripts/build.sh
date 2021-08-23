07392f61648d5233f2263bfbe15515b58480f15d
Galitan-dev <galitan.dev@gmail.com>
2021-08-23 10:38:00 +0200

    fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,5 @@ CWD=$(dirname "$(readlink -f "$0")")
 $CWD/stop.sh $NAME
 docker build -t $NAME:latest --build-arg path=$HOME/hosts/$NAME $HOME/heberg/app/images/node/
 
-mongo heberg --eval "db.hebergs.updateOne({ name: '$NAME' }, { $set: { containerId: null  } })" --quiet
+mongo heberg --eval "db.hebergs.updateOne({name:'$NAME'},{\$set:{containerId: null}})" --quiet
 $CWD/start.sh $NAME
