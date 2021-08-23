f633cfa9abc18ff70e63d5d6d6a81bf541a9f764
Galitan-dev <galitan.dev@gmail.com>
2021-08-23 19:10:50 +0200

    fixs

--- images/node/deploy.sh
+++ images/node/deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-rm $HOME/stdout.log $HOME/stderr.log
-touch $HOME/stdout.log
-touch $HOME/stderr.log
-exec > "stdout.log" 2> "stderr.log"
+STDOUT="$HOME/stdout.log"
+STDERR="$HOME/stderr.log"
+
+rm "$STDOUT" && touch "$STDOUT"
+rm "$STDERR" && touch "$STDERR"
+exec > "$STDOUT" 2> "$STDERR"
 
 eval "$(jq -r '.env | keys[] as $k | "export \($k)=\(.[$k]) &&"' package.json) cd ."
 
-cd ./app
+cd /app
 
 yarn install --production
 npm start
