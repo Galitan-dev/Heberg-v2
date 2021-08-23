7a682bbe179d1c03c07cafaab3f9c5ff9672007f
Galitan-dev <galitan.dev@gmail.com>
2021-08-23 13:10:00 +0200

    fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@ NAME=$1
 ID=$(mongo heberg --eval "db.hebergs.findOne({name:'$NAME'}).containerId" --quiet)
 
 if [ "$ID" == "null" ]; then
-    ID=$(docker run -d $NAME:latest)
+    ID=$(docker run -d -v $HOME/hosts/$NAME:/$NAME $NAME:latest)
     mongo heberg --eval "db.hebergs.updateOne({name:'$NAME'},{\$set:{containerId:'$ID'}})" --quiet
 else
     docker start $ID
